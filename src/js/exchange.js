981fb016a53a48fa5fbf249be6d2a6fd1506536a
James Provance <jb24x7@gmail.com>
2023-02-21 02:49:11 -0800

    add function to convert currencies

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -2,12 +2,28 @@ export default class Exchange {
   static async genApi() {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`);
-      const jsonApi = await response.json();
+      const json = await response.json();
       if (!response.result === 'success') {
         const errorMessage = `${response.result} ${response['error-type']}`;
         throw new Error(errorMessage);
       }
-      return jsonApi;
+      return json;
+    } catch (error) {
+      return error;
+    }
+  }
+  static async newAmount(currency1, currency2, amount) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`);
+      const json2 = await response.json();
+      if (!response.ok) {
+        let errorMessage = `${response.status}. Reason:${response.statusText} ${json2['error-type']}.`;
+        if (jsonifiedResponse['extra-info']) {
+          errorMessage = errorMessage.concat(` ${jsonifiedResponse['extra-info']}`);
+        }
+        throw new Error(errorMessage);
+      }
+      return jsonifiedResponse;
     } catch (error) {
       return error;
     }
