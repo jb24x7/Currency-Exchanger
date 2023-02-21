7356c30b0af5f86b6c326520563d07ec1014540c
James Provance <jb24x7@gmail.com>
2023-02-21 03:24:06 -0800

    (WIP) getting dropdowns to populate properly

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import Exchange from '../src/js/exchange';
+import Exchange from '../src/js/exchange';
 
-function getTypes(response, dropDown) {
+function getCurencies(response, dropDown) {
   response.supported_codes.forEach(element => {
     let option = document.createElement('option');
     option.innerText = element[0];
     option.setAttribute("value", `${element[0]}`)
     dropDown.append(option);
   });
+}
+
+function fillOptions(response) {
+  let from = document.getElementById('convertFrom');
+  let to = document.getElementById('convertTo');
+  getCurencies(response, from);
+  getCurencies(response, to);
+}
+
+
+
+window.onload = function() {
+  fillOptions(Exchange.genCurrencies());
+  Exchange.latestValue();
 }
