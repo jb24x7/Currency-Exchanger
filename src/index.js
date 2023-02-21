36325ae3766ed659435c05025a4c8194890a8b39
James Provance <jb24x7@gmail.com>
2023-02-21 03:28:46 -0800

    no longer will auto populate dropdown

--- src/index.js
+++ src/index.js
@@ -3,25 +3,3 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from '../src/js/exchange';
 
-function getCurencies(response, dropDown) {
-  response.supported_codes.forEach(element => {
-    let option = document.createElement('option');
-    option.innerText = element[0];
-    option.setAttribute("value", `${element[0]}`)
-    dropDown.append(option);
-  });
-}
-
-function fillOptions(response) {
-  let from = document.getElementById('convertFrom');
-  let to = document.getElementById('convertTo');
-  getCurencies(response, from);
-  getCurencies(response, to);
-}
-
-
-
-window.onload = function() {
-  fillOptions(Exchange.genCurrencies());
-  Exchange.latestValue();
-}
