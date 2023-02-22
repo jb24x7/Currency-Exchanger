e1709b46a5efd8ec4db89865c7f4758e36ee10a5
James Provance <jb24x7@gmail.com>
2023-02-21 19:07:14 -0800

    function that will be used to fill dropdowns and results

--- src/index.js
+++ src/index.js
@@ -22,3 +22,38 @@ function printError(error) {
   document.getElementById('text').innerText = `An error occured: ${error}`;
 }
 
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+
+async function genCurrencies() {
+  if (sessionStorage.getItem('supportedCurrencies')) {
+    const response = JSON.parse(sessionStorage.getItem('supportedCurrencies'));
+    fillOptions(response);
+  } else {
+    const response = await Exchange.genCurrencies();
+    if (response.result === 'success') {
+      sessionStorage.setItem('supportedCurrencies', JSON.stringify(response));
+      fillOptions(response);
+    } else {
+      printError(response);
+    }
+  }
+}
+
+export async function newAmount(amount, from, to) {
+  const response = await Exchange.newAmount(from, to, amount);
+  if (response instanceof Error) {
+    printError(response);
+  } else {
+    let result = response.conversion_result;
+    showAmount(amount, from, result, to);
+  }
+}
