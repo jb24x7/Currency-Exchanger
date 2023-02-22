54e34c67750630dc4deb76267410aa315439cf6e
James Provance <jb24x7@gmail.com>
2023-02-21 18:15:52 -0800

    add error function and fixing dropdowns

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from '../src/js/exchange';
 
+function getDropdown(response, dropDown) {
+  response.supported_codes.forEach(element => {
+    let option = document.createElement('option');
+    option.innerText = element[0];
+    dropDown.append(option);
+  });
+}
+
+function fillOptions(response) {
+  let from = document.getElementById('convertFrom');
+  let to = document.getElementById('convertTo');
+  getCurencies(response, from);
+  getCurencies(response, to);
+}
+
+function printError(error) {
+  document.getElementById('text').innerText = `An error occured: ${error}`;
+}
+
